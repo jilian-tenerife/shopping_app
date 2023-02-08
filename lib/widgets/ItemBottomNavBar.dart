7516588d09ca5dec175d000096041f7cc21dba93
Jillian <jillian.tenerife@usjr.edu.ph>
2023-02-09 02:48:54 +0800

    total and add to cart button added

--- lib/widgets/ItemBottomNavBar.dart
+++ lib/widgets/ItemBottomNavBar.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ItemBottomNavBar extends StatelessWidget {
   const ItemBottomNavBar({super.key});
@@ -15,6 +15,34 @@ class ItemBottomNavBar extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "\$120",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4C53A5)),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(CupertinoIcons.cart_badge_plus),
+              label: const Text(
+                "Add to Cart",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xFF4C53A5)),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(vertical: 13, horizontal: 15)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)))),
+            ),
+          ],
         ),
       ),
     );
