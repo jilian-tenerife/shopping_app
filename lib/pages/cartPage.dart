5c31dbc1ad7eda41bc9e66897bebd026500553c6
Jillian <jillian.tenerife@usjr.edu.ph>
2023-02-09 01:20:49 +0800

    cart items

--- lib/pages/cartPage.dart
+++ lib/pages/cartPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 import '../widgets/CartAppBar.dart';
+import '../widgets/CartItemSample.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -12,6 +13,20 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       body: ListView(children: [
         CartAppBar(),
+        Container(
+          height: 700,
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color: Color(0xffedecf2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            children: [CartItemSamples()],
+          ),
+        ),
       ]),
     );
   }
